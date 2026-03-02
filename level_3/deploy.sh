bb2372e94180883e71e22c4f2c6aff0cbaa50f4b
xbill <xbill9@gmail.com>
2026-03-02 23:14:07 +0000

    scripts

---
+++ level_3/deploy.sh
@@ -0,0 +1,13 @@
+export PROJECT_ID=$(cat ~/project_id.txt)
+export REGION=us-central1
+export SERVICE_NAME=biometric-scout
+export IMAGE_PATH=gcr.io/${PROJECT_ID}/${SERVICE_NAME}
+gcloud run deploy ${SERVICE_NAME} \
+  --image=${IMAGE_PATH} \
+  --platform=managed \
+  --region=${REGION} \
+  --allow-unauthenticated \
+  --set-env-vars="GOOGLE_CLOUD_PROJECT=${PROJECT_ID}" \
+  --set-env-vars="GOOGLE_CLOUD_LOCATION=${REGION}" \
+  --set-env-vars="GOOGLE_GENAI_USE_VERTEXAI=True" \
+  --set-env-vars="MODEL_ID=gemini-live-2.5-flash-preview-native-audio-09-2025"
